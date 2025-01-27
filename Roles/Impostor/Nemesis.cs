ec4079fae2bf3dee00bbb1d7cf4ed73a09cd6119
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-26 20:12:27 -0700

    Update Nemesis.cs

--- Roles/Impostor/Nemesis.cs
+++ Roles/Impostor/Nemesis.cs
@@ -175,7 +175,7 @@ internal class Nemesis : RoleBase
             }
             target.SetRealKiller(pc);
 
-            _ = new LateTask(() => { Utils.SendMessage(string.Format(GetString("NemesisKillSucceed"), Name), 255, Utils.ColorString(Utils.GetRoleColor(CustomRoles.Nemesis), GetString("NemesisRevengeTitle")), true); }, 0.6f, "Nemesis Kill");
+            _ = new LateTask(() => { Utils.SendMessage(string.Format(GetString("NemesisKillSucceed"), Name), 255, CustomRoles.Nemesis.GetColoredTextByRole(GetString("NemesisRevengeTitle")), true); }, 0.6f, "Nemesis Kill");
         }, 0.2f, "Nemesis Start Kill");
         return true;
     }
