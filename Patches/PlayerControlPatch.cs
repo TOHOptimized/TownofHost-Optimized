282a875ef50496a69273c7318789754ec67dc13f
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-29 07:36:21 -0700

    Update PlayerControlPatch.cs

--- Patches/PlayerControlPatch.cs
+++ Patches/PlayerControlPatch.cs
@@ -90,7 +90,7 @@ class CheckMurderPatch
 
         if (CheckForInvalidMurdering(killer, target, true) == false)
         {
-            killer.RpcMurderPlayer(target, error: true);
+            killer.RpcMurderPlayer(target, didSucceed: false);
             return false;
         }
 
@@ -564,7 +564,7 @@ class RpcMurderPlayerPatch
             return false;
         }
 
-        MurderResultFlags murderResultFlags = didSucceed ? MurderResultFlags.Succeeded : MurderResultFlags.FailedError;
+        MurderResultFlags murderResultFlags = didSucceed ? MurderResultFlags.Succeeded : MurderResultFlags.FailedError | MurderResultFlags.DecisionByHost;
         if (AmongUsClient.Instance.AmClient)
         {
             __instance.MurderPlayer(target, murderResultFlags);
@@ -575,7 +575,7 @@ class RpcMurderPlayerPatch
         AmongUsClient.Instance.FinishRpcImmediately(messageWriter);
 
         return false;
-        // There is no need to include DecisionByHost. DecisionByHost will make client check protection locally and cause confusion.
+        // There is no need to include DecisionByHost in succeeded kill attempt. DecisionByHost will make client check protection locally and cause confusion
     }
 }
 
