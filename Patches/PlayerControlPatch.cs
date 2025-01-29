955d94c3f2ab8cfdcbc259ee0a1d9fefed26fcbd
PEPPERcula <matthew.d.schlecht@gmail.com>
2025-01-29 14:31:44 -0700

    Update PlayerControlPatch.cs

--- Patches/PlayerControlPatch.cs
+++ Patches/PlayerControlPatch.cs
@@ -1287,7 +1287,7 @@ class FixedUpdateInNormalGamePatch
             //Mini's count down needs to be done outside if intask if we are counting meeting time
             if (GameStates.IsInGame && player.GetRoleClass() is Mini min)
             {
-                if (!player.Data.IsDead)
+                if (!player.Data.Disconnected)
                     min.OnFixedUpdates(player, nowTime);
             }
 
